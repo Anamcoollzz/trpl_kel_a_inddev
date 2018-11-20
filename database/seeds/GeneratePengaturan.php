449c62a7e4699a5e9f00f36ed1e722a24094c5e6
Anamcoollzz <hairulanam21@gmail.com>
2018-11-21 01:11:33 +0700

    generate pengaturan

--- database/seeds/GeneratePengaturan.php
+++ database/seeds/GeneratePengaturan.php
@@ -13,7 +13,8 @@ class GeneratePengaturan extends Seeder
     public function run()
     {
     	$pengaturan = '[{"id":1,"key":"email","value":"developer@inddev.com"},{"id":2,"key":"no_telp","value":"(0331) 8888 8888"},{"id":3,"key":"alamat","value":"<p>Jl Kalimantan, Jember<\/p>\r\n\t\t\t\t\t\t<p>Jawa Timur, Ind<\/p>"},{"id":4,"key":"privacy-policy","value":"<p><b>Ini halaman privacy policy<\/b><br><\/p>Blabla"}]';
-    	$pengaturan = json_encode($pengaturan);
+    	$pengaturan = json_decode($pengaturan);
+    	// dd($pengaturan);
     	foreach ($pengaturan as $p) {
     		Pengaturan::updateOrCreate([
     			'key'=>$p->key,
